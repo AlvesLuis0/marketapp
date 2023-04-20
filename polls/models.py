e418bfcce695e5428b77b234ce4af94eead5ac14
= <=>
2023-04-20 18:17:33 -0300

    tabelas sql criadas

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class CLIENT(models.Model):
+  CPF = models.IntegerField(primary_key=True)
+  NAME = models.CharField(max_length=50)
+  PHONE = models.CharField(max_length=22)
+  REGISTERED = models.DateTimeField()
+
+  def __str__(self):
+  	return self.NAME
+
+class PRODUCT(models.Model):
+  ID = models.AutoField(primary_key=True)
+  NAME = models.CharField(max_length=50)
+  PRICE = models.DecimalField(max_digits=6, decimal_places=2)
+  QUANTITY = models.IntegerField(default=0)
+  REGISTERED = models.DateTimeField()
+
+  def __str__(self):
+  	return self.NAME
