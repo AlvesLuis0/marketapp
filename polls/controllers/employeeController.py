ea57172fe89898121509173289e723e810e6030d
AlvesLuis0 <alves.luis.3002@gmail.com>
2023-05-29 22:00:38 -0300

    atualização e remoção de funcionários implementada

--- polls/controllers/employeeController.py
+++ polls/controllers/employeeController.py
@@ -29,3 +29,48 @@ def registerEmployee(request):
 	
 	messages.success(request, f"Funcionário '{user.username}' cadastrado com sucesso")
 	return render(request, "employee/register-employee.html")
+
+@user_passes_test(lambda user: user.is_superuser, "/login")
+def getAllEmployees(request):
+	employees = User.objects.all()
+	return render(request, "employee/all-employees.html", {
+		"employees": employees
+	})
+
+@user_passes_test(lambda user: user.is_superuser, "/login")
+def getEmployee(request, pk):
+	return render(request, "employee/update-employee.html", {
+		"employee": User.objects.get(pk=pk)
+	})
+
+@user_passes_test(lambda user: user.is_superuser, "/login")
+def updateEmployee(request, pk):
+	try:
+		user = User.objects.get(pk=pk)
+		user.username = request.POST["username"]
+		user.email = request.POST["email"]
+		user.set_password(request.POST["password"])
+
+		if request.POST["role"] == "staff":
+			user.is_staff = True
+		
+		elif request.POST["role"] == "admin":
+			user.is_staff = True
+			user.is_superuser = True
+
+		user.save()
+		messages.success(request, f"'{user.username}' atualizado com sucesso!")
+	
+	except:
+		messages.error(request, "Erro ao atualizar funcionário")
+	
+	return redirect(f"/employee/{pk}")
+
+@user_passes_test(lambda user: user.is_superuser, "/login")
+def deleteEmployee(request, pk):
+	try:
+		User.objects.get(pk=pk).delete()
+	
+	except: pass
+
+	return redirect("/employee")
--- polls/urls.py
+++ polls/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
   path("product/update/<int:pk>/", productController.updateProduct, name="updateProduct"),
   path("product/delete/<int:pk>/", productController.deleteProduct, name="deleteProduct"),
   path("product/<int:pk>/", productController.getProduct, name="getProduct"),
+  path("employee/", employeeController.getAllEmployees, name="getAllEmployees"),
   path("employee/register/", employeeController.registerEmployee, name="registerEmployee"),
+  path("employee/update/<int:pk>/", employeeController.updateEmployee, name="updateEmployee"),
+  path("employee/delete/<int:pk>/", employeeController.deleteEmployee, name="deleteEmployee"),
+  path("employee/<int:pk>/", employeeController.getEmployee, name="getEmployee"),
   path("login/", authController.loginController, name="login"),
   path("logout/", authController.logoutController, name="logout")
 ]
