032caa09d9e8fdc1f2837cb7ea07bd41c4cd40c2
AlvesLuis0 <alves.luis.3002@gmail.com>
2023-06-02 18:49:23 -0300

    pronto para o uso

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-clear
-set -xe
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py createsuperuser --username="admin" --email="admin@gmail.com"
-python3 manage.py runserver
