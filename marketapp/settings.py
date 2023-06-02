c174c77cfc17e85286bf4ae4d89406012cf78c12
AlvesLuis0 <alves.luis.3002@gmail.com>
2023-06-02 16:46:55 -0300

    permição de outros hosts adicionada

--- marketapp/settings.py
+++ marketapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+8h)&jfr939js7zit9$q@-7w4ozsww#yf6&@j8$#5s(cj81hu&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
